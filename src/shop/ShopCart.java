5688ed3b4c2e258305cb228aa8aa89356805ea37
sivagudur <siva.sgudur@gmail.com>
2025-11-20 11:37:20 +0000

    Added documentation

--- src/shop/ShopCart.java
+++ src/shop/ShopCart.java
@@ -7,6 +7,16 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
+ * Shopping cart
+ * This Java Program builds a checkout system for a shop which only sells apples and oranges
+ * • Apples cost 60p and oranges cost 25p
+ * • Build a checkout system which takes a list of items scanned at the till and outputs the total cost
+ * - For example: [ Apple, Apple, Orange, Apple ] => £2.05
+ *
+ * • This program also caters two new offers in checkout system
+ * - Buy one, get one free on apples
+ * - 3 for the price of 2 on oranges
+ *
  *
  */
 public class ShopCart {
